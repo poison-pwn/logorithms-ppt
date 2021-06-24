332976a22f8fb2666d25adf86b6e71aad6be0a38
NEONSOLISALPHA <45178461+NEONSOLISALPHA@users.noreply.github.com>
2021-06-24 12:03:47 +0530

    added ShowExpression Scene to illustrate relationship between base exponent and argument

--- main.py
+++ main.py
@@ -23,3 +23,65 @@ class Intro(Scene):
         intro_q.target.scale(0.8)
         self.play(FadeIn(intro_ans, shift=UP), MoveToTarget(intro_q))
         self.play(*[FadeOut(i, shift=UP) for i in self.mobjects])
+
+
+class ShowExpression(Scene):
+    def construct(self):
+        equat_basic = MathTex("{{2}}{{^3}}={{8}}")
+        equat_basic.scale(4)
+        self.play(Write(equat_basic))
+        equat_transformed = MathTex("{{log}}{{^{}_2}}{{(}}{{8}}{{)}}={{3}}").scale(4)
+        changes = [(0, 1), (2, 5), (3, 3)]
+        self.play(
+            FadeIn(equat_transformed[0], shift=RIGHT),
+            *[FadeIn(equat_transformed[i], shift=UP) for i in [2, 4]],
+            *[
+                ReplacementTransform(equat_basic[i], equat_transformed[j])
+                for i, j in changes
+            ],
+            ClockwiseTransform(equat_basic[1], equat_transformed[6]),
+        )
+        self.remove(*self.mobjects)
+        equat_copy = equat_transformed.copy()
+        self.add(equat_copy)
+        self.play(Indicate(equat_copy[1]))
+        self.play(Indicate(equat_copy[3]))
+        self.play(Indicate(equat_copy[6]))
+        self.remove(*self.mobjects)
+        equat_resplit = MathTex("{{log^{}_2(8)}}={{3}}").scale(4)
+        equat_resplit.save_state()
+        equat_final = MathTex("2^{", r"log^{}_2(8)}", "=", "8").scale(4)
+        self.add(equat_resplit)
+        changes = [
+            (0, 1),
+            (1, 2),
+            (-1, -1),
+        ]
+        self.play(
+            FadeIn(equat_final[0], shift=UP),
+            *[Transform(equat_resplit[i], equat_final[j]) for i, j in changes],
+        )
+        self.play(Restore(equat_resplit), FadeOut(equat_final[0]))
+        self.remove(equat_resplit)
+        equat_resplit_for_e = MathTex("{{log}}{{^{}_2}}{{(8)}}{{=3}}").scale(4)
+        equat_with_e = MathTex("{{log}}{{^{}_e}}{{(8)}}{{=2.794}}").scale(4)
+        self.play(
+            *[
+                ReplacementTransform(equat_resplit_for_e[i], equat_with_e[i])
+                for i in range(len(equat_with_e))
+            ]
+        )
+        self.remove(*self.mobjects)
+        equat_with_e_copy = MathTex("{{l}}{{og^{}_e}}{{(8)=2.794}}").scale(4)
+        self.add(equat_with_e_copy)
+        equat_with_ln = MathTex("{{l}}{{n}}{{(8)}}{{=2.794}}").scale(4)
+        equat_with_e_copy[-1].generate_target()
+        equat_with_e_copy[-1].target.next_to(equat_with_ln[1], RIGHT, buff=SMALL_BUFF)
+
+        self.play(
+            ReplacementTransform(equat_with_e_copy[0], equat_with_ln[0]),
+            FadeOut(equat_with_e_copy[1], shift=UP),
+            FadeIn(equat_with_ln[1], shift=UP),
+            MoveToTarget(equat_with_e_copy[-1]),
+        )
+        self.play(*[FadeOut(i, shift=UP) for i in self.mobjects])
